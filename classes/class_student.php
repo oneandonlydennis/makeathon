6d9b9ed673a6cee6a08c324f03c78cd7c38b2c6a
Yassinefahmi <2>
2019-06-06 00:00:17 +0200

    Array toegevoegd aan class student

--- classes/class_student.php
+++ classes/class_student.php
@@ -11,17 +11,16 @@
             for($y=1; $y<10; $y++){
                 $rand1 = rand(1,10);
                 $rand2 = rand(1,10);
-                echo $rand1." x ".$rand2." = <input type='number' name='sum".$y."'><br>";
-                $this->sommen = [$rand1, $rand2, $rand1*$rand2];
+                $form = '$rand1' . ' x ' . $rand2 . ' = <input type=\'number\' name=\'sum".$y."\'><br>"';
+                return $form;
+                $this->sommen[] = [$rand1, $rand2, $rand1*$rand2];
             }
         }
 
         public function resultaat(){
-            return $this->sommen[0][1];
+            if ($this->sommen[0][2]) {
+                return 'Test';
+            }
         }
-    } 
-
-    $kind = new Student;
-    $kind->resultaat();
-    
+    }
 ?>
