d5606f7e5e193df9e8cda4b63ac1c0ce99e08bcf
Yassinefahmi <2>
2019-06-06 03:55:34 +0200

    Sommen in array geplaats en preview van de declaring ervan

--- classes/class_student.php
+++ classes/class_student.php
@@ -8,19 +8,32 @@
         }
 
         public function multiply(){
-            for($y=1; $y<10; $y++){
-                $rand1 = rand(1,10);
-                $rand2 = rand(1,10);
-                $form = "$rand1 X $rand2 = <input type='number' name='num$y'><br>";
-                echo $form;
-                $this->sommen[] = [$rand1, $rand2, $rand1*$rand2];
+            for($y=0; $y<20; $y++){
+                $rand1 = rand(1, 9);
+                $rand2 = rand(1, 9);
+                $this->sommen[] = [$rand1 * $rand2];
+
+                if ($y <= 5) {
+                    $form = "<div class='eerste-helft col-3'>$rand1 X $rand2 = <input type='number' name='num$y'><br></div>";
+                    echo $form;
+                } else if ($y >= 5) {
+                    $form = "<div class='tweede-helft col-3'>$rand1 X $rand2 = <input type='number' name='num$y'><br></div>";
+                    echo $form;
+                } else if ($y >= 10) {
+                    $form = "<div class='tweede-helft col-3'>$rand1 X $rand2 = <input type='number' name='num$y'><br></div>";
+                    echo $form;
+                } else if ($y >= 15) {
+                    $form = "<div class='tweede-helft col-3'>$rand1 X $rand2 = <input type='number' name='num$y'><br></div>";
+                    echo $form;
+                }
             }
         }
 
-        public function resultaat(){
-            if ($this->sommen[0][2]) {
-                return 'Test';
-            }
+        public function arrayresult() {
+            return var_dump($this->sommen[5][0]);
+//            if ($this->sommen[0] == $sum1 && $this->sommen[1] == $sum2) {
+//                return true;
+//            }
         }
     }
 ?>
--- student/sommen.php
+++ student/sommen.php
@@ -1,21 +1,29 @@
 <?php
-    //require("../includes/autoloader.php");
+    session_start();
     require("../classes/class_student.php");
+
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
 ?>
 
 <html>
     <head>
         <link rel="stylesheet" href="../css/style.css">
         <link rel="stylesheet" href="../css/bootstrap.css">
+        <link rel="stylesheet" href="../css/sommen.css">
     </head>
     <body>
         <div class="card" id="studentcard">
             <div class="card-body" id="sums">
-                <form method="post">
+                <form method="POST">
+<!--                    <div class="row">-->
                     <?php
                         $kid = new Student;
+                        $kid->arrayresult();
                     ?>
-                <button type="submit" class="btn btn-primary">Bereken!</button>
+<!--                    </div>-->
+                    <button name="berekenen" type="submit" class="btn btn-primary">Klik hier om je toets in te leveren</button>
                 </form>
             </div>
         </div>
